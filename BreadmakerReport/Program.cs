0658de6c9f0d37184657cc1ce6fe2585dc2da481
Percy Mittagadapala <willi.percy3@gmail.com>
2021-07-31 23:45:39 -0400

    Assignment 1 Part B

---
+++ BreadmakerReport/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using RatingAdjustment.Services;
+using BreadmakerReport.Models;
+
+namespace BreadmakerReport
+{
+    class Program
+    {
+        static string dbfile = @".\data\breadmakers.db";
+        static RatingAdjustmentService ratingAdjustmentService = new RatingAdjustmentService();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Bread World");
+            var BreadmakerDb = new BreadMakerSqliteContext(dbfile);
+            
+            //Fetch the data into List
+            var BMList = BreadmakerDb.Breadmakers
+                .Select(breadmaker => new {
+                            ID = breadmaker.BreadmakerId,
+                            CalReviews = breadmaker.Reviews.Count,
+                            CalAverage = breadmaker.Reviews.Average(x => x.stars),
+                            CalAdjust = ratingAdjustmentService.Adjust(breadmaker.Reviews.Average(x => x.stars),breadmaker.Reviews.Count),
+                            Description = breadmaker.title
+                        })
+                .AsEnumerable()
+                .OrderByDescending(x => x.CalAdjust)
+                .ToList();
+
+            //Display the list data
+            Console.WriteLine("[#]  Reviews Average  Adjust    Description");
+            for (var j = 0; j < 3; j++)
+            {
+                var i = BMList[j];
+                Console.WriteLine($"[{j+1}] {i.CalReviews,8} {i.CalAverage,-2:F2} {i.CalAdjust,8:F2}      {i.Description}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RatingAdjustmentService/RatingAdjustmentService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace RatingAdjustment.Services
+{
+    /** Service calculating a star rating accounting for the number of reviews
+     * Percy
+     */
+    public class RatingAdjustmentService
+    {
+        const double MAX_STARS = 5.0;  // Likert scale
+        const double Z = 1.96; // 95% confidence interval
+
+        double _q;
+        double _percent_positive;
+
+        /** Percentage of positive reviews
+         * 
+         * In this case, that means X of 5 ==> percent positive
+         * 
+         * Returns: [0, 1]
+         */
+        void SetPercentPositive(double stars)
+        {
+            this._percent_positive = stars / RatingAdjustmentService.MAX_STARS;
+        }
+
+        /**
+         * Calculate "Q" given the formula in the problem statement
+         */
+        void SetQ(double number_of_ratings)
+        {
+            double result = ((this._percent_positive * (1 - this._percent_positive)) + (Math.Pow(RatingAdjustmentService.Z, 2) / (4 * number_of_ratings))) / number_of_ratings;
+            this._q = RatingAdjustmentService.Z * Math.Sqrt(result);
+        }
+
+        /** Adjusted lower bound
+         * 
+         * Lower bound of the confidence interval around the star rating.
+         * 
+         * Returns: a double, up to 5
+         */
+        public double Adjust(double stars, double number_of_ratings)
+        {
+            //Calling the above two defined methods to get the result
+            this.SetPercentPositive(stars);
+            this.SetQ(number_of_ratings);
+
+            double result = (this._percent_positive + (Math.Pow(RatingAdjustmentService.Z, 2) / (2 * number_of_ratings)) - this._q) / (1 + (Math.Pow(RatingAdjustmentService.Z, 2) / number_of_ratings)) * RatingAdjustmentService.MAX_STARS;
+            return result;
+        }
+    }
+}
